dbeaa9c10ce5151fa9cc82ebe33f6f45e87b3c75
albertgoncalves <goncalves.albert@gmail.com>
2019-04-11 22:43:28 -0400

    first commit

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs; mkShell {
+    name = "Haskell";
+    buildInputs = [
+        (haskell.packages.ghc864.ghcWithPackages (pkgs: [
+            pkgs.cairo
+            pkgs.containers
+            pkgs.hlint
+            pkgs.hoogle
+            pkgs.HUnit
+            pkgs.random
+            pkgs.tf-random
+        ]))
+    ];
+    shellHook = ''
+        if [ $(uname -s) = "Darwin" ]; then
+            alias ls='ls --color=auto'
+            alias ll='ls -al'
+        fi
+    '';
+}
