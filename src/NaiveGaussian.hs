1ed2acdb530a123f311974b299a46023ce05a5fb
albertgoncalves <goncalves.albert@gmail.com>
2019-04-13 10:35:31 -0400

    scalability

--- src/NaiveGaussian.hs
+++ src/NaiveGaussian.hs
@@ -9,45 +9,45 @@ import GaussianPDF (applyGPDF)
 import Prelude hiding (lookup)
 import Utils (equalLength, mapTuple, seqTuple)
 
-transform :: (Ord a, Floating a) => [a] -> [([a], b)] -> Maybe (a, b)
-transform x = seqTuple . (\(a, b) -> (f x a, listToMaybe b)) . unzip
+transform :: (Ord a, Floating a) => [a] -> [(b, [a])] -> Maybe (b, a)
+transform x = seqTuple . (\(b, a) -> (listToMaybe b, f x a)) . unzip
   where
     f :: (Ord a, Floating a) => [a] -> [[a]] -> Maybe a
     f x' =
         (exp . sum . map log <$>)
         . (zipWithM (flip applyGPDF) x' . transpose)
 
-probability :: (Eq a, Floating a) => (a, Bool) -> (a, Bool) -> Maybe a
-probability (x, True) (y, False) = Just $ x / (x + y)
-probability (y, False) (x, True) = Just $ x / (x + y)
-probability (0, _) (0, _) = Nothing
+probability :: (Eq a, Floating a) => (Bool, a) -> (Bool, a) -> Maybe a
+probability (True, x) (False, y) = Just $ x / (x + y)
+probability (False, y) (True, x) = Just $ x / (x + y)
+probability (_, 0) (_, 0) = Nothing
 probability _ _ = Nothing
 
 classify
     :: (Eq a, Ord a, Floating a)
-    => [([a], Bool)]
+    => [(Bool, [a])]
     -> [a]
     -> Maybe a
-classify xs x = if equalLength (x:map fst xs) then f xs else Nothing
+classify xs x = if equalLength (x:map snd xs) then f xs else Nothing
   where
     f =
         (uncurry probability =<<)
         . seqTuple
         . mapTuple (transform x)
-        . partition snd
+        . partition fst
 
 main :: IO ()
-main = (print . classify xs) (fst x)
+main = (print . classify xs) (snd x)
   where
     xs =
-        [ ([0.62657841, 16.849859], True)
-        , ([-0.03280966, 16.791390], True)
-        , ([0.88748101, 4.655336], True)
-        , ([0.65862394, 8.828321], True)
-        , ([5.304892, -7.1470574], False)
-        , ([1.114964, -0.3882628], False)
-        , ([20.660083, 7.2656835], False)
-        , ([26.430639, -6.6080169], False)
-        , ([40.653835, -1.2885520], False)
-        ] :: [([Float], Bool)]
-    x = ([1.76782283, 7.797349], True) :: ([Float], Bool)
+        [ (True, [0.62657841, 16.849859])
+        , (True, [-0.03280966, 16.791390])
+        , (True, [0.88748101, 4.655336])
+        , (True, [0.65862394, 8.828321])
+        , (False, [5.304892, -7.1470574])
+        , (False, [1.114964, -0.3882628])
+        , (False, [20.660083, 7.2656835])
+        , (False, [26.430639, -6.6080169])
+        , (False, [40.653835, -1.2885520])
+        ] :: [(Bool, [Float])]
+    x = (True, [1.76782283, 7.797349]) :: (Bool, [Float])
