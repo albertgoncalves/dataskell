c92dc7ea635b84fa8d04c97590221ab53bba29c2
albertgoncalves <goncalves.albert@gmail.com>
2019-04-13 12:56:13 -0400

    applicatives

--- src/Utils.hs
+++ src/Utils.hs
@@ -2,13 +2,14 @@
 
 module Utils where
 
+import Control.Applicative (liftA2)
 import Data.Function (on)
 
 mapTuple :: (a -> b) -> (a, a) -> (b, b)
 mapTuple = uncurry . on (,)
 
-seqTuple :: Monad m => (m a, m b) -> m (a, b)
-seqTuple (a, b) = a >>= \a' -> (,) a' <$> b
+seqTuple :: (Applicative f) => (f a, f b) -> f (a, b)
+seqTuple = uncurry $ liftA2 (,)
 
 equalLength :: [[a]] -> Bool
 equalLength = and . (zipWith (==) <*> drop 1) . map length
