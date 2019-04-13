6f94e08b3b43fec5b85c9552eef7b676a58e4a1a
albertgoncalves <goncalves.albert@gmail.com>
2019-04-13 10:30:13 -0400

    rename

--- shell.nix
+++ shell.nix
@@ -3,13 +3,9 @@ with pkgs; mkShell {
     name = "Haskell";
     buildInputs = [
         (haskell.packages.ghc864.ghcWithPackages (pkgs: [
-            pkgs.cairo
-            pkgs.containers
             pkgs.hlint
             pkgs.hoogle
             pkgs.HUnit
-            pkgs.random
-            pkgs.tf-random
         ]))
     ];
     shellHook = ''
