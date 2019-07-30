1140b31135535b537e1ea17198f819646355329b
albertgoncalves <goncalves.albert@gmail.com>
2019-07-29 21:06:21 -0400

    mkShell update

--- shell.nix
+++ shell.nix
@@ -1,6 +1,5 @@
-{ pkgs ? import <nixpkgs> {} }:
-with pkgs; mkShell {
-    name = "dataskell";
+with import <nixpkgs> {};
+mkShell {
     buildInputs = [
         (haskell.packages.ghc865.ghcWithPackages (pkgs: [
             pkgs.hindent
