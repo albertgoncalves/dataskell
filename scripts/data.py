fa020fefe15dc1a3010983c8164e93dfc6bd061b
albertgoncalves <goncalves.albert@gmail.com>
2019-11-27 11:06:49 -0500

    update python style, no-op

--- scripts/data.py
+++ scripts/data.py
@@ -5,31 +5,22 @@ from sys import argv
 from pandas import DataFrame
 from sklearn.datasets import make_blobs, make_circles, make_moons
 
-from utils import path, pipe
+from path import path
 
 
 def extract(xyz):
-   return {"z": xyz[1], "x": xyz[0].T[0], "y": xyz[0].T[1]}
-
-
-def export(filename):
-    def f(df):
-        df.to_csv(filename, index=False)
-    return f
+    return {"z": xyz[1], "x": xyz[0].T[0], "y": xyz[0].T[1]}
 
 
 def main():
-    dataset = \
-        { "blobs": make_blobs(n_features=2, centers=2, cluster_std=4)
-        , "circles": make_circles(noise=0.2, factor=0.5)
-        , "moons": make_moons(noise=0.3)
-        }
+    dataset = {
+        "blobs": lambda: make_blobs(n_features=2, centers=2, cluster_std=4),
+        "circles": lambda: make_circles(noise=0.2, factor=0.5),
+        "moons": lambda: make_moons(noise=0.3),
+    }
     try:
-        pipe( dataset[argv[1]]
-            , extract
-            , DataFrame
-            , export(path("data", "input.csv"))
-            )
+        DataFrame(extract(dataset[argv[1]]())) \
+            .to_csv(path("data", "input.csv"), index=False)
     except:
         args = (argv[0], "|".join(dataset.keys()))
         exit("{} unknown argument, valid arguments: {}".format(*args))
--- scripts/plot.py
+++ scripts/plot.py
@@ -5,32 +5,25 @@ from sys import argv
 from matplotlib.pyplot import close, savefig, subplots, tight_layout
 from pandas import read_csv
 
-from utils import path
-
-
-def render_plot(input_, output):
-    _, ax = subplots(figsize=(5, 5))
-    kwargs = {"cmap": "bwr", "vmin": 0, "vmax": 1}
-    ax.tricontourf(output.x, output.y, output.z, alpha=0.75, **kwargs)
-    ax.scatter(input_.x, input_.y, c=input_.z, edgecolor="w", **kwargs)
-    ax.set_aspect("equal")
-
-
-def export_plot(path):
-    savefig(path)
-    tight_layout()
-    close()
+from path import path
 
 
 def main():
     try:
         input_ = read_csv(path("data", "input.csv"))
-        output = read_csv( path("data", "output.csv")
-                         , header=None
-                         , names=["z", "x", "y"]
-                         )
-        render_plot(input_, output)
-        export_plot(path("out", "plot.png"))
+        output = read_csv(
+            path("data", "output.csv"),
+            header=None,
+            names=["z", "x", "y"],
+        )
+        _, ax = subplots(figsize=(5, 5))
+        kwargs = {"cmap": "bwr", "vmin": 0, "vmax": 1}
+        ax.tricontourf(output.x, output.y, output.z, alpha=0.75, **kwargs)
+        ax.scatter(input_.x, input_.y, c=input_.z, edgecolor="w", **kwargs)
+        ax.set_aspect("equal")
+        savefig(path("out", "plot.png"))
+        tight_layout()
+        close()
     except:
         exit("{} unable to plot data".format(argv[0]))
 
